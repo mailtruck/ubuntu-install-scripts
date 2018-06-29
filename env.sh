123d3262d09f2319fc76cdffd015f00277c58993
Brian Danowski <briandanowski@gmail.com>
2018-06-28 23:34:42 -0400

    Adds ubuntu deps install to env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-sudo apt-get install zsh curl -y
+sudo apt-get update --fix-missing
+sudo apt-get install zsh curl make build-essential python-dev libxml2-dev libxslt-dev vim-gui-common -y
+sudo apt-get update --fix-missing
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+./install_go192.sh
+./install_node893.sh
+source $HOME/.zshrc
+cd ..
+git clone https://github.com/mailtruck/vim-env.git
+cd vim-env
+make
